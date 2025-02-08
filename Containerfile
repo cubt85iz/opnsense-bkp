b2bd658281a20bee96b7c00302ec72c0df7c6673
Philip Hoffman <git@cubt85iz>
2025-02-08 14:39:00 -0500

    Adds container definition.

--- Containerfile
+++ Containerfile
@@ -1,3 +1,14 @@
-FROM docker.io/library/alpine:latest
+FROM docker.io/library/alpine:3.21
 
+ENV OPNSENSE_USER_KEY=
+ENV OPNSENSE_USER_SECRET=
+ENV OPNSENSE_URI=
 
+RUN apk add --no-cache curl
+
+RUN mkdir /config
+VOLUME /config
+
+COPY src/* /
+WORKDIR /
+ENTRYPOINT ["/download-backup.sh /config/"]
---
+++ src/download-backup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env ash
+
+set -euox pipefail
+
+if [ -z "$OPNSENSE_USER_KEY" ]; then
+  echo "ERROR: User key was not provided."
+  exit 1
+fi
+
+if [ -z "$OPNSENSE_USER_SECRET" ]; then
+  echo "ERROR: User secret was not provided."
+  exit 1
+fi
+
+if [ -z "$OPNSENSE_DOMAIN" ]; then
+  echo "ERROR: Domain for opnsense was not provided."
+  exit 1
+fi
+
+
+DATETIME=$(date +%Y%m%d-%H%M%S)
+curl -u "${OPNSENSE_USER_KEY}:${OPNSENSE_USER_SECRET}" \
+  "https://${OPNSENSE_DOMAIN}/api/core/backup/download/this" \
+  -o "/config/${DATETIME}-${OPNSENSE_DOMAIN}-backup.xml"
